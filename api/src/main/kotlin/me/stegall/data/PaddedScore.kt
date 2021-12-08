23bf3a36aa50f868482bab356219e4930405290e
Sam Stegall <stegall@medicine.tamhsc.edu>
2021-12-08 15:41:25 -0600

    Setup beautiful scores route.

--- api/src/main/kotlin/me/stegall/data/PaddedScore.kt
+++ api/src/main/kotlin/me/stegall/data/PaddedScore.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class PaddedScore(
   val purchase_price: Float,
+  val average_score: Float,
   val values: ArrayList<Score>,
 )
--- api/src/main/kotlin/me/stegall/plugins/Serialization.kt
+++ api/src/main/kotlin/me/stegall/plugins/Serialization.kt
@@ -5,7 +5,6 @@ import com.github.kittinunf.result.Result
 import io.ktor.serialization.*
 import io.ktor.features.*
 import io.ktor.application.*
-import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.serialization.*
@@ -16,6 +15,18 @@ import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+
+fun average(values: ArrayList<Float>): Float {
+  var counter = 0.0f
+  for (value in values) {
+    counter += value
+  }
+  return counter / values.size
+}
+
+fun generate_score(original: Float, current: Float): Float {
+  return current / original
+}
 
 fun Application.configureSerialization() {
   val values = Values()
@@ -96,6 +107,7 @@ fun Application.configureSerialization() {
     }
     get("/scores") {
       val scoresMap = HashMap<String, PaddedScore>()
+      val valuesMap = HashMap<String, ArrayList<Value>>()
       if (call.request.headers["APCA-API-KEY-ID"] != null && call.request.headers["APCA-API-SECRET-KEY"] != null) {
         val (_request, _response, result) = "https://paper-api.alpaca.markets/v2/positions"
           .httpGet()
@@ -132,7 +144,7 @@ fun Application.configureSerialization() {
               }
               is Result.Success -> {
                 val data = result.get()
-                val allOrders = Json.decodeFromString<ArrayList<Order>>(data).filter {
+                val orders = Json.decodeFromString<ArrayList<Order>>(data).filter {
                   var present = false
                   for (position in minifiedPositions) {
                     if (position.symbol == it.symbol) {
@@ -141,15 +153,73 @@ fun Application.configureSerialization() {
                   }
                   present
                 }
-                for (position in minifiedPositions) {
-                  val startDates = arrayOf(
-                    OffsetDateTime.now().minusMonths(6).format(DateTimeFormatter.ISO_DATE_TIME),
-                    OffsetDateTime.now().minusMonths(3).format(DateTimeFormatter.ISO_DATE_TIME),
-                    OffsetDateTime.now().minusMonths(1).format(DateTimeFormatter.ISO_DATE_TIME),
-                    OffsetDateTime.now().minusWeeks(2).format(DateTimeFormatter.ISO_DATE_TIME),
-                    OffsetDateTime.now().minusWeeks(1).format(DateTimeFormatter.ISO_DATE_TIME),
-                    OffsetDateTime.now().minusDays(1).format(DateTimeFormatter.ISO_DATE_TIME),
+                for (order in orders) {
+                  val filled_date = OffsetDateTime.parse(order.filled_at)
+                  val queryDateTimes = arrayOf(
+                    filled_date.plusMinutes(15).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusMinutes(30).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusHours(1).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusHours(4).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusDays(1).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusWeeks(1).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusWeeks(2).format(DateTimeFormatter.ISO_DATE_TIME),
+                    filled_date.plusMonths(1).format(DateTimeFormatter.ISO_DATE_TIME),
                   )
+                  for (queryDateTime in queryDateTimes) {
+                    val (_request, _response, result) = "https://data.alpaca.markets/v2/stocks/${order.symbol}/trades?start=${queryDateTime}&limit=1"
+                      .httpGet()
+                      .header("APCA-API-KEY-ID", call.request.headers["APCA-API-KEY-ID"]!!)
+                      .header("APCA-API-SECRET-KEY", call.request.headers["APCA-API-SECRET-KEY"]!!)
+                      .responseString()
+                    when (result) {
+                      is Result.Failure -> {
+                        val ex = result.getException()
+                        call.respondText(ex.toString())
+                      }
+                      is Result.Success -> {
+                        val historicalData = Json.decodeFromString<StockResponse>(result.get())
+                        val trade = historicalData.trades.get(0)
+                        if (!valuesMap.containsKey(order.symbol)) {
+                          valuesMap.put(order.symbol, arrayListOf(Value(order.symbol, trade.s, trade.p, trade.t)))
+                        } else {
+                          val valueFromMap = valuesMap.get(order.symbol)
+                          if (valueFromMap != null) {
+                            valueFromMap.add(Value(order.symbol, trade.s, trade.p, trade.t))
+                            valuesMap.put(order.symbol, valueFromMap)
+                          }
+                        }
+                      }
+                    }
+                    valuesMap.keys.forEach {
+                      val original_price = order.filled_avg_price
+                      val values = valuesMap.get(it)
+                      val scores_for_symbol = ArrayList<Score>()
+                      if (values != null) {
+                        for (value in values) {
+                          if (original_price != null) {
+                            scores_for_symbol.add(
+                              Score(
+                                order.symbol,
+                                value.current_price,
+                                generate_score(original_price.toFloat(), value.current_price),
+                                value.time
+                              )
+                            )
+                          }
+                        }
+                        if (original_price != null) {
+                          val values_for_average = ArrayList<Float>()
+                          scores_for_symbol.forEach {
+                            values_for_average.add(it.score)
+                          }
+                          scoresMap.put(
+                            order.symbol,
+                            PaddedScore(original_price.toFloat(), average(values_for_average), scores_for_symbol)
+                          )
+                        }
+                      }
+                    }
+                  }
                 }
                 call.respond(scoresMap)
               }

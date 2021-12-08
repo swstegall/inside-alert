665459b9a87a2452d33591262448505e0838195a
Sam Stegall <stegall@medicine.tamhsc.edu>
2021-12-08 15:55:35 -0600

    Setup querying for scores.

---
+++ ui/src/individualComponents/ScoresSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ScoresSection = ({ scores }) => {
+  return (
+    <div className={"row pt-4"}>
+      <div className={"col"}>Scores</div>
+    </div>
+  );
+};
+
+export default ScoresSection;
